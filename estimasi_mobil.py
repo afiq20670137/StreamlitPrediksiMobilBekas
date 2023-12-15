47a9e8a160180fc60e5c7bb3e38ce08f2ecd039b
afiq20670137 <97424681+afiq20670137@users.noreply.github.com>
2023-12-16 01:33:09 +0700

    Add files via upload

---
+++ estimasi_mobil.py
@@ -0,0 +1,24 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('estimasi_mobil.sav', 'rb'))
+
+
+st.title('Estimasi Harga Mobil Bekas')
+
+year = st.number_input('Tahun mobil')
+mileage = st.number_input('Kilometer mobil')
+tax = st.number_input('Pajak mobil')
+mpg = st.number_input('konsumsi BBM')
+engineSize = st.number_input('ukuran mesin')
+
+predict = ''
+if st.button('estimasi harga'):
+    predict = model.predict(
+        [[year, mileage, tax, mpg, engineSize]]
+    )
+    st.write('estimasi harga mobil bekas dalam pounds : ', predict)
+    st.write('estimasi harga mobil bekas dalam IDR : ', predict*19000)
+    
+    
+    st.text('Created By Afiq & Niken')
